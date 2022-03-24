53cd6ac22cc9aced8d9530382d8c3d47d2d7fd17
Adam Finkle <Adam.Finkle001@umb.edu>
2022-03-24 13:32:22 -0400

    Moving code around

--- analysis.py
+++ analysis.py
@@ -1,23 +1,6 @@
 from scipy.stats import hypergeom
 
 
-def p_value(selected_column,
-            remaining_column,
-            selected_column_count,
-            remaining_column_count):
-    """
-    Return the sum of the hypergeometric probabilities of more 
-    favorable results in the same number of trials.
-    """
-    selected, remaining = len(selected_column), len(remaining_column)
-    favorable = selected_column_count + remaining_column_count
-    remaining_unfavorable = remaining - remaining_column_count
-    trials = selected + remaining
-    limit = favorable + max(remaining_unfavorable - favorable, 0)
-    possibilities = [i for i in range(favorable, limit)]
-    return sum(hypergeom.pmf(possibilities, trials, favorable, selected))
-
-
 def column_letter_counts(column):
     """Return an array of counts of letters in an amino acid column."""
     letter_counts = [0 for _ in range(ord("Y"))]
--- p_values.py
+++ p_values.py
@@ -2,6 +2,23 @@ import analysis
 import sequence_search
 
 
+def p_value(selected_column,
+            remaining_column,
+            selected_column_count,
+            remaining_column_count):
+    """
+    Return the sum of the hypergeometric probabilities of more 
+    favorable results in the same number of trials.
+    """
+    selected, remaining = len(selected_column), len(remaining_column)
+    favorable = selected_column_count + remaining_column_count
+    remaining_unfavorable = remaining - remaining_column_count
+    trials = selected + remaining
+    limit = favorable + max(remaining_unfavorable - favorable, 0)
+    possibilities = [i for i in range(favorable, limit)]
+    return sum(hypergeom.pmf(possibilities, trials, favorable, selected))
+
+
 def p_values(selected_sequences, remaining_sequences):
     """
     Return the p-values of the count of every amino acid letter in each columnar
@@ -15,8 +32,8 @@ def p_values(selected_sequences, remaining_sequences):
     remaining_letter_counts = analysis.letter_counts(remaining_sequences)
     for i in range(columns)
         for letter in letters:
-            p_values[i].append((selected_sequences[i],
-                                remaining_sequences[i],
-                                selected_column_counts[i],
-                                remaining_column_count[i]))
+            p_values[i].append(p_value(selected_sequences[i],
+                                       remaining_sequences[i],
+                                       selected_column_counts[i],
+                                       remaining_column_count[i]))
     return p_values
