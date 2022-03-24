ca236f63bdbac91faee790dc8d3a2a307d943074
Adam Finkle <Adam.Finkle001@umb.edu>
2022-03-24 13:28:23 -0400

    Even more stuff

--- sequence_search.py
+++ sequence_search.py
@@ -1,44 +1,14 @@
-import mmap
-
-
-def read_gene_list(filename = "uniprot_sprot.fasta"):
-    with open(filename) as f:
-        return f.read().split(">")
-
-    
-def extract_names_and_sequences(gene_list):
-    names, sequences = [], []
-    greatest_name_length = 0
-    greatest_sequence_length = 0
-    for gene_entry in gene_list[1:]:
-        lines = gene_entry.split("\n")
-        gene_annotation = lines[0]
-        if "OS=Human" not in gene_annotation: continue
-        if "GN=" not in gene_annotation: continue
-        else: gene_name = gene_annotation.split("GN=")[1].split(" ")[0]
-        fasta_sequence = "".join(lines[1:])
-        names.append(gene_name)
-        sequences.append(fasta_sequence)
-    return names, sequences
-        
-    
-def save_names_and_sequences(names, sequences):
-    names_and_indices = [[name, n] for n, name in enumerate(names)]
-    names_and_indices.sort()
-    with open('raw_sequences.txt', 'w') as sequences_file, open('gene_names.txt', 'w') as names_file:
-        for name, index in names_and_indices:
-            names_file.write(name + "\n")
-            sequences_file.write(sequences[index] + "\n")
-        
-            
-def get_sequence(gene_name):
-    with open('gene_names.txt') as gene_names:
-        for i, name in gene_names:
-            if name == gene_name:
-                sequence_number = i
-                break
-    with open ('raw_sequences.txt') as sequences:
-        for i, sequence in sequences:
-            if i == sequence_number:
-                return sequence
-        
+def windowed_sequence(gene_name, site_number, window_width):
+    """
+    Return a view of sequences aligned by phosphorylation site and limited by
+    a window width to either site of it.
+    """
+    site_index, site_count = 0, 0
+    sequence = sequence_search.get_sequence(gene_name)
+    windowed_sequence = []
+    for i, letter in enumerate(sequence):
+        if letter.upper() == "S": site_count += 1
+        if site_count == site_number: site_index = i; break
+    for i in range(site_index - window_width, site + window_width):
+        windowed_sequence.append(sequence[i])
+    return windowed_sequence
