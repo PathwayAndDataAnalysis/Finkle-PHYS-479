1a70a80718bbef5ef255418b021f17018effbb44
Adam Finkle <Adam.Finkle001@umb.edu>
2022-04-11 17:32:26 -0400

    fixed

--- processing.py
+++ processing.py
@@ -1,4 +1,6 @@
 def substitute_amino_acids(sequences):
     with open("amino-acid-mapping.tsv") as f:
-        substitutions = {line.split("\t")[0] : line.split("\t")[1] for line in f.readlines()}
-    return [[substitutions[character] for character in lines] for sequence in sequences]
+        substitutions = {line.split("\t")[0] : line.split("\t")[1][:-1]
+                         for line in f.readlines()}
+    return ["".join([substitutions[amino_acid] for amino_acid in sequence])
+             for sequence in sequences]
