719901fd6da62e7c66504cfa0ab588d9a6e5efdf
Adam Finkle <Adam.Finkle001@umb.edu>
2022-04-14 15:25:06 -0400

    added new method

--- p_values.py
+++ p_values.py
@@ -25,3 +25,47 @@ def sequence_p_values(selected_sequences, remaining_sequences):
             enrichment_p_values[column].append(scipy.stats.fisher_exact(table, 'less')[1])
             deficiency_p_values[column].append(scipy.stats.fisher_exact(table, 'greater')[1])
     return enrichment_p_values, deficiency_p_values
+
+
+def most_significant_p_values(sequences, column, letter, favorable):
+    """
+    Return the p-values of least absolute magnitude calculated by Fisher's
+    exact test on a list of windowed sequences ranked by phenotypic p-value from
+    most least positive to greatest positive to least negative to greatest
+    negative, first by sliding a threshold down from greatest positive and then
+    by sliding up from least negative.
+    """
+    greatest_negative_p_value, least_positive_p_value = -1, 0
+    selected_favorable, selected_unfavorable = 0, 0
+    remaining_favorable = favorable
+    remaining_unfavorable = len(sequences) - favorable
+    for selected, sequence in enumerate(reversed(sequences)):
+        is_favorable = sequence[column] == letter
+        remaining_unfavorable -= !is_favorable
+        remaining_favorable -= is_favorable
+        selected_unfavorable += !is_favorable
+        selected_favorable += is_favorable
+        table = [[selected_unfavorable, selected_favorable],
+                 [remaining_unfavorable, remaining_favorable]]
+        deficiency_p_value = scipy.fisher_exact(table, alternative = 'less')
+        greatest_negative_p_value = max(greatest_negative_p_value,
+                                        deficiency_p_value)
+        
+    selected_favorable, selected_unfavorable = 0, 0
+    remaining_favorable = favorable
+    remaining_unfavorable = len(sequences) - favorable
+    for selected, sequence in enumerate(sequences):
+        is_favorable = sequence[column] == letter
+        remaining_unfavorable -= !is_favorable
+        remaining_favorable -= is_favorable
+        selected_unfavorable += !is_favorable
+        selected_favorable += is_favorable
+        table = [[selected_unfavorable, selected_favorable],
+                 [remaining_unfavorable, remaining_favorable]]
+        enrichment_p_value = scipy.fisher_exact(table, alternative = 'greater')
+        least_positive_p_value = min(least_positive_p_value,
+                                     enrichment_p_value)
+        
+    return greatest_negative_p_value, least_positive_p_value
+        
+   
