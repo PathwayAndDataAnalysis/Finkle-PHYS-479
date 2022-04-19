4e8e4fbc7b97a683bfb07ffa1922c568651c9731
Adam Finkle <Adam.Finkle001@umb.edu>
2022-04-19 19:21:26 -0400

    Update p_values.py

--- p_values.py
+++ p_values.py
@@ -11,8 +11,8 @@ def sequence_p_values(selected_sequences, remaining_sequences):
     letters = ["A", "G", "I", "L", "P", "V", "F", "W", "Y", "D", 
                "E", "R", "H", "K", "S", "T", "C", "M", "N", "Q"]
     columns = len(selected_sequences)
-    enrichment_p_values = [[] for _ in columns]
-    deficiency_p_values = [[] for _ in columns]
+    enrichment_p_values = [[] for _ in range(columns)]
+    deficiency_p_values = [[] for _ in range(columns)]
     remaining_rows = len(remaining_sequences[0])
     selected_rows = len(selected_sequences[0])
     selected_letter_counts = analysis.letter_counts(selected_sequences)
