e2d75badc0b3be5717d44f0489d87386bf19fd84
Adam Finkle <Adam.Finkle001@umb.edu>
2022-04-21 19:33:38 -0400

    removed superflous method

--- p_values.py
+++ p_values.py
@@ -3,31 +3,6 @@ import sequence_search
 from scipy.stats import fisher_exact
 
 
-def sequence_p_values(selected_sequences, remaining_sequences):
-    """
-    Return the p-values of the count of every amino acid letter in each columnar
-    position of aligned rows of amino acid sequences selected from a corpus.
-    """
-    letters = ["A", "G", "I", "L", "P", "V", "F", "W", "Y", "D", 
-               "E", "R", "H", "K", "S", "T", "C", "M", "N", "Q"]
-    columns = len(selected_sequences)
-    enrichment_p_values = [[] for _ in range(columns)]
-    deficiency_p_values = [[] for _ in range(columns)]
-    remaining_rows = len(remaining_sequences[0])
-    selected_rows = len(selected_sequences[0])
-    selected_letter_counts = analysis.letter_counts(selected_sequences)
-    remaining_letter_counts = analysis.letter_counts(remaining_sequences)
-    for column in selected_sequences:
-        for letter in letters:
-            table = [[remaining_rows - remaining_letter_counts[column],
-                     remaining_letter_counts[column]],
-                     [selected_rows - selected_letter_counts[column],
-                     selected_letter_counts[column]]]
-            enrichment_p_values[column].append(fisher_exact(table, 'less')[1])
-            deficiency_p_values[column].append(fisher_exact(table, 'greater')[1])
-    return enrichment_p_values, deficiency_p_values
-
-
 def most_significant_p_values(sequences, index, letter, total_favorable):
     """
     Return the p-values of least absolute magnitude calculated by Fisher's
